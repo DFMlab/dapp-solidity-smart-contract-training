ac7e47031503850fc696727165be8974ca22c6b8
iamoracle <officialbilas@gmail.com>
2022-04-22 13:46:23 +0100

    added hello world contract 3

--- HelloWorld/3_helloworld.sol
+++ HelloWorld/3_helloworld.sol
@@ -4,7 +4,7 @@
 /**
  * @title Hello World Solidity Smart Contract
  * @dev A Simple Hello World Solidity Smart Contract 
- * for demonstrating constructor and str memory.
+ * for demonstrating public visibility and bool.
  * @author Decentralized Future in Motion Lab Limited
  */
 
@@ -16,29 +16,24 @@ contract HelloWorld {
 
     /// @notice A variable that holds the name
     /// @dev A state variable that holds an string value (name)
-    string _name;
+    /// This variable doesn't need a getter anymore
+    /// @return string
+    string public _name;
+
+    bool _active;
 
     /// @notice The constructor is a function that is called when the contract is first deployed
     /// @dev a constructor to initialize the name variable
     /// @param name the name is a local variable help in the memory
-   constructor(string memory name) {
+    constructor(string memory name, bool active) {
         _name = name; // local variable
+        _active = active; // local variable
     }
 
-    /// @notice a function that gets a name
-    /// @dev a function that gets the state variable name.
-    /// This is a public function can be called internally and externally.
-    /// a view function will not modify the state of the blockchain
-    /// @return string memory from the state variable that    
-    function getName() public view returns(string memory) {
-        return _name;
-    }
-
-
+   
     function setAmount(uint amount) public {
         _amount = amount; // amount is a local variable
     }
-
 
     function getAmount() public view returns(uint256) {
         return _amount;
