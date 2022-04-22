9d83cce5da8bd2c44dbd35c289a475803d813af6
iamoracle <officialbilas@gmail.com>
2022-04-22 13:37:54 +0100

    Improved comment

--- HelloWorld/1_helloworld.sol
+++ HelloWorld/1_helloworld.sol
@@ -12,17 +12,23 @@ pragma solidity ^0.8.3;
 
 contract HelloWorld {
 
+    /// @notice A variable that holds the amount
+    /// @dev A state variable that holds an integer value (amount)
     uint256 _amount; // state variable
 
-    // a function that sets amount
-    // a public function uses more gas than external
-    // a public function can be called internally and externally
+    /// @notice a function that sets the amount on blockchain
+    /// @dev a function that sets the state variable amount.
+    /// This is a public function can be called internally and externally
+    /// @param amount the value to be set
     function setAmount(uint amount) public {
         _amount = amount; // amount is a local variable
     }
 
-    // return the amount
-    // a view function will not modify any state variable
+    /// @notice a function that gets the amount stored on the blockchain
+    /// @dev a function that gets the state variable amount.
+    /// This is a public function can be called internally and externally.
+    /// a view function will not modify the state of the blockchain
+    /// @return uint256 from the state variable that
     function getAmount() public view returns(uint256) {
         return _amount;
     }
--- HelloWorld/2_helloworld.sol
+++ HelloWorld/2_helloworld.sol
@@ -12,31 +12,34 @@ pragma solidity ^0.8.3;
 
 contract HelloWorld {
 
-    uint256 _amount; // state variable
+    uint256 _amount;
 
-    string _name; // state variable
+    /// @notice A variable that holds the name
+    /// @dev A state variable that holds an string value (name)
+    string _name;
 
-    // The constructor is called when the smart is deployed
-    // a constructor to initialize the name variable
-    // a string is stored in memory in form of an array
-    constructor(string memory name) {
+    /// @notice The constructor is a function that is called when the contract is first deployed
+    /// @dev a constructor to initialize the name variable
+    /// @param name the name is a local variable help in the memory
+   constructor(string memory name) {
         _name = name; // local variable
     }
 
-    // a getter function for the name variable
+    /// @notice a function that gets a name
+    /// @dev a function that gets the state variable name.
+    /// This is a public function can be called internally and externally.
+    /// a view function will not modify the state of the blockchain
+    /// @return string memory from the state variable that    
     function getName() public view returns(string memory) {
         return _name;
     }
 
-    // a function that sets amount
-    // a public function uses more gas than external
-    // a public function can be called internally and externally
+
     function setAmount(uint amount) public {
         _amount = amount; // amount is a local variable
     }
 
-    // return the amount
-    // a view function will not modify any state variable
+
     function getAmount() public view returns(uint256) {
         return _amount;
     }
