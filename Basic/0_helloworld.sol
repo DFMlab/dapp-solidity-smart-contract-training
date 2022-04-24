9e62183ee499c3e0971808aed9331c1e24d9e809
iamoracle <officialbilas@gmail.com>
2022-04-24 11:38:22 +0100

    UPDATE: Added Hello World Contract

---
+++ Basic/0_helloworld.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: GPL-3.0-only
+// Decentralized Future in Motion Limited Training Contracts v0.0.1 (Basic/0_helloworld.sol)
+
+/**
+ * @title A Simple Hello World Solidity Smart Contract
+ * @dev A Simple Hello World Solidity Smart Contract
+ * for demonstrating how solidity works.
+ * @author Decentralized Future in Motion Lab Limited
+ */
+
+pragma solidity ^0.8.3;
+
+/// @title Hello World contract very similar to classes in C and C++
+/// @dev Hello World contract
+contract HelloWorld {
+
+    function helloWorld() public pure returns(str) {
+        return "hello world";
+    }
+
+}
