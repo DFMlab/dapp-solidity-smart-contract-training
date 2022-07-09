e88ccf197a9360daa06b22928042bfeebf497771
iamoracle <officialbilas@gmail.com>
2022-07-09 22:46:04 +0100

    :tada: added project and boilerplate

---
+++ Project/contract/tree.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract TreeProject {
+
+    struct Tree {
+        uint256 price;
+        address payable owner;
+        string image;
+    }
+
+    mapping(uint256 => Tree) public _trees;
+
+    uint256 _treeCount;
+
+    function addTree(uint256 price, string memory image) external {
+        _trees[_treeCount].price = price;
+        _trees[_treeCount].owner = payable(msg.sender);
+        _trees[_treeCount].image = image;
+        _treeCount++;
+    }
+
+    function buyTree(uint256 id) external payable {
+        require(msg.value == _trees[id].price, "invalid amount");
+        _trees[id].owner.transfer(msg.value);
+        _trees[id].owner = payable(msg.sender);
+        _trees[id].price +=  _trees[id].price / 10; 
+    }
+
+}
